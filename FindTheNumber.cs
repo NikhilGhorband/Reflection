e5f1fa899820202f0ad0157b16e09ddb6416761b
NikhilGhorband <127277360+NikhilGhorband@users.noreply.github.com>
2023-07-08 01:09:36 +0530

    FetchAllClass

---
+++ FindTheNumber.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReflectionProblem2
+{
+    public class FindTheNumber
+    {
+        public static void ReflectionInfo()
+        {
+            Type type = typeof(FindTheNumber);
+
+            
+            Console.WriteLine("FullClass Name is: " + type.FullName);
+           
+
+            Console.WriteLine("ClassName is: " + type.Name + "\n");
+
+            Console.WriteLine("Methods used in Class Customer is as follows");
+            
+            MethodInfo[] methods = type.GetMethods();
+
+            
+            foreach (MethodInfo method in methods)
+            {
+                Console.WriteLine("Method Name: " + method.Name + " " + "Method RetrunType: " + method.ReturnType.Name);
+            }
+
+            
+            Console.WriteLine("\nProperties of Class FindTheNumber");
+
+            PropertyInfo[] properties = type.GetProperties();
+
+            
+            foreach (PropertyInfo property in properties)
+            {
+                Console.WriteLine(property.PropertyType.Name + ": " + property.Name);
+            }
+
+            
+            Console.WriteLine("\nConstructor Present in FindTheNumber class");
+
+            ConstructorInfo[] constructors = type.GetConstructors();
+            foreach (ConstructorInfo constructor in constructors)
+            {
+                Console.WriteLine(constructor.ToString());
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+
+namespace ReflectionProblem2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+         FindTheNumber.ReflectionInfo();
+        }
+    }
+}
